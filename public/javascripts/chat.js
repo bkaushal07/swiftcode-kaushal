f4a99b885712f3d080354817dd52c9a223cfc671
bkaushal07 <bankaushal@gmail.com>
2018-04-14 12:18:24 +0000

    frontend for chatbot

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,1 +1,22 @@
+var app = angular.module('chatApp', ['ngMaterial']);
 
+app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hi'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'Hello whats up?'
+	},
+        {
+            'sender': 'USER',
+            'text': 'IPL'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'ESCN'
+	}
+	];
+});
